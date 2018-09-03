3207d8ec3f7a4d7702c1b35906a6f93bf2568dc9
Fariha Tarannum <Fariha_Tarannum@gap.com>
2018-09-03 12:30:43 +0530

    SQL Automation

---
+++ SSASDeployment/SSASDeployment/BuildProject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AnalysisServices.Tabular;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+using Microsoft.Build.BuildEngine;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
+using Microsoft.Build.Framework;
+
+namespace SSASDeployment
+{
+    class BuildProject
+    {
+        public static void Main(string[] args)
+        {
+            /***************** Code to Build the Solution in local GitHub directory ************************/
+            /*
+            string projectFilePath = Path.Combine(@"C:\Buyplan\TabularCubes\Buyplan BR\Buyplan BR\Buyplan BR.smproj");
+            // Instantiate a new FileLogger to generate build log
+            FileLogger logger = new FileLogger();
+            ProjectCollection pc = new ProjectCollection();            
+            Dictionary<string, string> globalProperty = new Dictionary<string, string>();
+            globalProperty.Add("OutputPath", @"c:\Users\ftarann\Documents\Temp");
+            globalProperty.Add("Configuration", "Deployment");
+            //globalProperty.Add("Platform", "Any CPU");
+            BuildRequestData buildRequest = new BuildRequestData(projectFilePath, globalProperty, "4.0", new string[] { "Build" }, null);
+            BuildParameters bp = new BuildParameters(pc);
+            bp.Loggers = new List<ILogger> { logger }.AsEnumerable();
+            try
+            {
+                BuildResult buildResult = BuildManager.DefaultBuildManager.Build(bp, buildRequest);
+                // A SIMPLE WAY TO CHECK THE RESULT
+                if (buildResult.OverallResult == BuildResultCode.Success)
+                {
+                    pc.UnregisterAllLoggers();
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            */
+            SQLDeployment sql = new SQLDeployment();
+            sql.publishChanges();
+            //SSISDeploymentCode ssis = new SSISDeploymentCode();
+            //string res = ssis.SSIS_Deploy();
+            //DeploymentCode ssas = new DeploymentCode();
+            //ssas.SSASDeploymentCode();
+            
+
+        }
+    }
+}
+
+
+
---
+++ SSASDeployment/SSASDeployment/SQLDeployment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Dts.Runtime;
+using System.Data.SqlClient;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.SqlServer.Dac;
+
+namespace SSASDeployment
+{
+    class SQLDeployment
+    {
+        public void publishChanges()
+        {
+            string dacpacPath = @"C:\Program Files (x86)\Jenkins\workspace\FirstDeploymentProject\SQLTestProject\SQLTestProject\bin\Debug\SQLTestProject.dacpac";
+            string dacProfilePath = @"C:\Program Files (x86)\Jenkins\workspace\FirstDeploymentProject\SQLTestProject\SQLTestProject\Dev.publish.xml";
+            var dp = DacPackage.Load(dacpacPath);
+            var dbDeployOptions = new DacDeployOptions();
+            dbDeployOptions.BlockOnPossibleDataLoss = false;
+            //dbDeployOptions.SqlCommandVariableValues.
+            //Load DacProfile from sql project
+            DacProfile d = new DacProfile();
+            d = DacProfile.Load(dacProfilePath);
+            //var p = new PublishOptions();                       
+            //p.GenerateDeploymentScript = true;
+            //p.DatabaseScriptPath = "";             
+            var dbServices = new DacServices(d.TargetConnectionString);
+            dbServices.Deploy(dp, d.TargetDatabaseName, true, dbDeployOptions);
+
+        }
+        
+    }
+}
